63ceebe9beb6dfb2e8a40511a1efe02ab4caaf4f
The-Parthib <rijupanja81@gmail.com>
2024-12-19 02:33:52 +0530

    Implement Armstrong number check in armStrong.java

--- Challenges/armStrong.java
+++ Challenges/armStrong.java
@@ -4,6 +4,19 @@ public class armStrong {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
         //int num = sc.nextInt();
+        int num = 153;
+        int temp = num;
+        int sum = 0;
+        while(num > 0){
+            int rem = num % 10;
+            sum += Math.pow(rem, 3);
+            num = num / 10;
+        }
+        if(sum == temp){
+            System.out.println("Armstrong Number");
+        }else{
+            System.out.println("Not Armstrong Number");
+        }
         sc.close();
 
     }
