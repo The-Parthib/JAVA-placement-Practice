dd81f6d14ca74af2334f552d4690101605f7da5e
The-Parthib <rijupanja81@gmail.com>
2024-12-25 01:49:08 +0530

    Add month array printing program in array03.java

---
+++ JAVA Basics/array03.java
@@ -0,0 +1,8 @@
+public class array03 {
+    public static void main(String[] args) {
+        String[] month = {"January","February","March","April","October","November","December"};
+        for (int i = 0; i < month.length; i++) {
+            System.out.println(month[i]);
+        }
+    }
+}
