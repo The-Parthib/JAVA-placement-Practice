712fa62aba7e6366b218a245a59445aece45532e
The-Parthib <rijupanja81@gmail.com>
2024-12-27 02:37:51 +0530

    nothing special

--- JAVA Basics/array02.java
+++ JAVA Basics/array02.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class array02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] myArr = new int[10];
+        int [] myArr = new int[5];
         String [] array = new String[2];
         array[0] = "Parthib";
         array[1] = "Rupsa";
+        System.out.println(array[0]);
         System.out.println(array[1]);
         int indx = 0;
         while (indx <myArr.length) {
@@ -15,7 +16,8 @@ public class array02 {
         }
         sc.close();
         //Array Traversal
-        indx = 9;
+        System.out.println("Array Traversal \n\n");
+        indx = 0;
         while (indx <myArr.length) {
             System.out.print(myArr[indx]+" ");
             indx++;
