0ae9331024b29c85457098030fb5b26e71c9aa3f
The-Parthib <rijupanja81@gmail.com>
2025-01-01 02:25:07 +0530

    Add Car class with fuel management and driver program

---
+++ Class and Objects/Car.java
@@ -0,0 +1,32 @@
+import java.nio.channels.Pipe.SourceChannel;
+
+public class Car {
+
+    //instance variables;
+    int noOfWheels;
+    String color;
+    float maxSpeed;
+    float currentFuelInLiters;
+    int noOfSeats;
+
+    // instance methods
+    public void drive(){
+        System.out.println("Car is being driven");
+        if (currentFuelInLiters == 0) { 
+            System.out.println("Please Fill Tank, Out of Fuel");
+        } else if (currentFuelInLiters < 5) {
+            System.out.println("Car is in Reserved Mode Please Fuel Up");
+            currentFuelInLiters--;
+        } else{
+            currentFuelInLiters--;
+        }
+    }
+
+    public void addFuel(float fuel){
+        currentFuelInLiters += fuel;
+    }
+
+    public float getCurrentFuel(){
+        return currentFuelInLiters;
+    }
+}
---
+++ Class and Objects/driver.java
@@ -0,0 +1,7 @@
+public class driver {
+ public static void main(String[] args) {
+    Car myCar = new Car();
+    myCar.drive();
+    System.out.println(myCar.getCurrentFuel());
+ }
+}
