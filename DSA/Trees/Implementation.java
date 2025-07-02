2d90f9740494bbfc3ff774e00abe4c05ad8614c6
The-Parthib <rijupanja81@gmail.com>
2025-07-02 22:37:04 +0530

    Add inorder, postorder, level, nth level, and level order traversal methods to Implementation class

--- DSA/Trees/Implementation.java
+++ DSA/Trees/Implementation.java
@@ -45,6 +45,25 @@ public class Implementation {
         preOrder(root.right);
     }
 
+    // Inorder
+    public static void inOrder(Node root){
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    //postOrder traversal
+
+    public static void postOrder(Node root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+" ");
+    }
+
     // Size of tree
     public static int size(Node root){
         if (root == null) {
@@ -92,10 +111,41 @@ public class Implementation {
         return (1+Math.max(height(root.left),height(root.right)));
     }
 
+    // level of tree
+
+    public static int level(Node root){
+        int result = height(root);
+        return (result+1);
+
+        // if (root==null || (root.left == null && root.right == null)){
+        //     return 0;
+        // }
+        // return 1+(1+Math.max(height(root.left),height(root.right)));
+    }
+
+    //n-th level of tree
+    public static void nthLevel(Node root, int level){
+        if(root==null) return;
+        if (level==1) {
+            System.out.print(root.data+" ");
+        }
+        nthLevel(root.left, level-1);
+        nthLevel(root.right, level-1);
+    }
+
     // Min value of the tree
     public static int minElement(Node root){
         if (root == null) return Integer.MAX_VALUE;
         return Math.min(root.data,Math.min(minElement(root.left), minElement(root.right)));
+    }
+    
+    // Level Order / BFS
+    public static void levelOrder(Node root){
+        int length = level(root);
+        for (int i = 1; i <= length; i++) {
+            nthLevel(root, i);
+            System.err.println();
+        }
     }
 
     public static void main(String[] args) {
@@ -111,13 +161,24 @@ public class Implementation {
         Node e = new Node(11);
         b.right = e;
         display(root);
-        preOrder(root);
+
+        // System.out.println("PreOrder traversal:");
+        // preOrder(root);
+        // System.out.println("\nInOrder traversal:");
+        // inOrder(root);
+        // System.out.println("\nPostOrder traversal:");
+        // postOrder(root);
 
         // System.out.println("size of tree : "+size(root));
         // System.out.println("sum of tree elements : "+sum(root));
         // System.out.println("Max of tree elements : "+maxElement(root));
         // System.out.println("Height of tree : "+height(root));
         // System.out.println("Min of tree : "+minElement(root));
-        System.out.println("mul of tree : "+mul(root));
+        // System.out.println("\nmul of tree : "+mul(root));
+        // System.out.println("\nlevel of tree : "+level(root));
+        // nthLevel(root, 3);
+        levelOrder(root);
+        
+        
     }
 }
