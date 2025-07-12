db94a8e7eb26241f267c2e1a2c3ebc4e2c00ebe3
The-Parthib <rijupanja81@gmail.com>
2025-07-12 20:58:50 +0530

    Add RecursionThings class with various recursive methods including printName, printNums, printRevNums, backTracking, devisor, and reversePrint

--- Recursion/RecursionThings.java
+++ Recursion/RecursionThings.java
@@ -1,11 +1,13 @@
 package Recursion;
 
-public class NamePrint {
+public class RecursionThings {
     public static void main(String[] args) {
-        printName(1, 5);
+        // printName(1, 5);
         // printNums(1, 5);
         // printRevNums(5, 5);
-        backTracking(5, 5);
+        // backTracking(5, 5);
+        // devisor(36);
+        reversePrint(1, 9);
     }
 
     public static void printName(int i,int n){
@@ -14,7 +16,6 @@ public class NamePrint {
         System.out.println("Riju");
         printName(i+1, n);
     }
-
     public static void printNums(int i, int n){
     if(i>n) return;
         System.out.print(i+" ");
@@ -27,7 +28,26 @@ public class NamePrint {
     }
     public static void backTracking(int i, int n){
     if(i<1) return;
-    backTracking(i-1, n);
+    backTracking(i-1, n); // not allowed (i+1,n)
     System.out.print(i+" ");
     }
+
+    public static void devisor(int n){
+        for (int i = 1; i*i<=n; i++) {
+            if (n%i == 0) {
+                System.out.print(i+" ");
+                if ((n/i)!=i) {
+                    System.out.print((n/i)+" ");
+                }
+            }
+        }
+    }
+      
+    public static void reversePrint(int i, int n){
+        if(i>n) return;
+
+        reversePrint(i+1, n);
+        System.out.print(i+" ");
+    }
+
 }
