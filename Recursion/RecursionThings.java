cea2f971106c0989430f65f2b96ef40e2c39ec75
The-Parthib <rijupanja81@gmail.com>
2025-07-12 23:38:13 +0530

    reverse array

--- Recursion/RecursionThings.java
+++ Recursion/RecursionThings.java
@@ -1,5 +1,7 @@
 package Recursion;
 
+import java.lang.reflect.Array;
+
 public class RecursionThings {
     public static void main(String[] args) {
         // printName(1, 5);
@@ -7,7 +9,18 @@ public class RecursionThings {
         // printRevNums(5, 5);
         // backTracking(5, 5);
         // devisor(36);
-        reversePrint(1, 9);
+        // reversePrint(1, 9);
+        // sumOfn(5, 0);
+        // int result = sumOfn(5);
+        // System.out.println(result);
+        // int fact = fact(4);
+        // System.out.println(fact);
+        int arr[] = {2,3,5,7,8};
+        reverseArray(0, arr, 5);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        
     }
 
     public static void printName(int i,int n){
@@ -50,4 +63,36 @@ public class RecursionThings {
         System.out.print(i+" ");
     }
 
+    public static void sumOfn(int i,int sum){
+        if (i<1) {
+            System.out.println(sum);
+            return;
+        }
+        sumOfn(i-1,sum+i);
+    }
+
+    public static int sumOfn(int n){
+        if (n == 0 ) {
+            return 0;
+        }
+        return n+sumOfn(n-1);
+    }
+
+    public static int fact(int n){
+        if (n==0 || n== 1) {
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+
+    public static void reverseArray(int i,int arr[],int n){
+        if (i >= n/2) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+
+        reverseArray(i+1, arr, n);
+    }
 }
