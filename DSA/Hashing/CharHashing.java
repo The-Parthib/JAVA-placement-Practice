10075af9beb14d0972dff32435e0ed907fcd6ab6
The-Parthib <rijupanja81@gmail.com>
2025-07-14 17:33:09 +0530

    Refactor CharHashing class to include main method and character counting functionality

--- DSA/Hashing/CharHashing.java
+++ DSA/Hashing/CharHashing.java
@@ -1,5 +1,22 @@
 package DSA.Hashing;
 
 public class CharHashing {
-    String s = "abcdefgh";
+    public static void main(String[] args) {
+        String s = "abcABdabcAAAxyzfe";
+        int result = countChar(s, 'A');
+        System.out.println(result);
+    }
+
+    public static int countChar(String s, char c){
+        int[] hash = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            // int j = s.charAt(i)-'a';
+            // int j = s.charAt(i)-'A';
+            hash[s.charAt(i)]++;  // all
+            // hash[j]++;
+        }
+        // return hash[c-'a']; // in case all are lower case
+        // return hash[c-'A']; // in case all Upper case
+        return hash[c]; // in case all are strings
+    }
 }
