60a29435a09b7e296cc8151317ed7f144dfde824
The-Parthib <rijupanja81@gmail.com>
2025-08-09 21:52:07 +0530

    Refactor RotateArray class to use static method for rotation; optimize array manipulation logic

--- DSA/Arrays/basics/RotateArray.java
+++ DSA/Arrays/basics/RotateArray.java
@@ -1,21 +1,48 @@
 package DSA.Arrays.basics;
+/*
+ * [1,2,3,4,5,6,7]
+ * rotate by k = 3 elements here
+ * [5,6,7,1,2,3,4] this is resultant array
+ * 
+ */
+
 
 public class RotateArray {
-    public void rotate(int[] nums, int k) {
+    public static void rotate(int[] nums, int k) {
         int n = nums.length;
+        k=k%n;
 
-        while(k>0){
-        int temp = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            nums[i+1] = nums[i];
+        // extract to temp array
+        int[] temp = new int[k];
+
+        for(int i=0;i<k;i++){
+            temp[i] = nums[n-k+i];
         }
-        nums[0] = temp;
-        k--;
+
+        // shifting
+        for(int i=n-k-1;i>=0;i--){
+            nums[i+k] = nums[i];
+        }
+
+        // re-build
+        for(int i=0;i<k;i++){
+            nums[i] = temp[i];
         }
     }
 
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+
+        System.out.println("original array : ");
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
         
+        System.out.println("\nrotated  array : ");
+        rotate(arr,3);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
     }
     
 }
