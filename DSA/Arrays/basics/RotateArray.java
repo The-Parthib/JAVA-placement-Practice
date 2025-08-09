0b2ff11dd57f00e712aa1af4a967447752675576
The-Parthib <rijupanja81@gmail.com>
2025-08-09 22:51:26 +0530

    Implement optimal array rotation method and refactor main method to use it

--- DSA/Arrays/basics/RotateArray.java
+++ DSA/Arrays/basics/RotateArray.java
@@ -30,6 +30,26 @@ public class RotateArray {
         }
     }
 
+    public static void reverse(int[] arr, int start, int end){
+        while (start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void optimalRotate(int[] nums,int k){
+        int n = nums.length;
+        k=k%n;
+        
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
 
@@ -39,7 +59,7 @@ public class RotateArray {
         }
         
         System.out.println("\nrotated  array : ");
-        rotate(arr,3);
+        optimalRotate(arr,3);
         for (int i : arr) {
             System.out.print(i+" ");
         }
