d167749fb6846f7f435588d60495e7c9f8c60ad9
The-Parthib <rijupanja81@gmail.com>
2025-08-27 23:22:58 +0530

    intersection

--- DSA/Arrays/question_topics/Intersection.java
+++ DSA/Arrays/question_topics/Intersection.java
@@ -6,24 +6,8 @@ public class Intersection {
 
     static ArrayList<Integer> intersectionOptimal(int[] arr1, int[] arr2){
 
-        int m = arr1.length;
-        int n = arr2.length;
-        int i=0,j=0;
-
         ArrayList<Integer> list = new ArrayList<>();
 
-        while (i<m && j<n) {
-            if(arr1[i]<arr2[j]){
-                i++;
-            }
-            else if(arr2[j]<arr1[i]){
-                j++;
-            }
-            else{
-                list.add(arr1[i]);
-                i++;
-            }
-        }
 
         return list;
     }
@@ -33,7 +17,6 @@ public class Intersection {
         int[] arr2 = { 1, 3, 5, 8 };
         // [1,3,5]
 
-        // unionBrute(arr1, arr2);
         ArrayList<Integer> result = intersectionOptimal(arr1, arr2);
 
         for (int i : result) {
