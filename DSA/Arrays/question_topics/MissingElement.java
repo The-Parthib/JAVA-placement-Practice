dcf5c75e12e3b4809b418aba96a3894876eb2507
The-Parthib <rijupanja81@gmail.com>
2025-08-28 23:51:58 +0530

    Add optimal and XOR methods for finding missing elements in an array

--- DSA/Arrays/question_topics/MissingElement.java
+++ DSA/Arrays/question_topics/MissingElement.java
@@ -35,8 +35,38 @@ public class MissingElement {
         return -1;
     }
 
+    static int findMissingelementOptimal(int[] arr){
+        // as the array is not containing 0 so take the length +1;
+
+        int n = arr.length+1;
+        int sum = (n*(n+1))/2;
+        int result = 0;
+        for (int i = 0; i < n-1; i++) {
+            result = result+arr[i];
+        }
+        result = sum-result;
+        return result;
+    }
+
+    static int findMissingelementXor(int[] arr){
+        int xor1 = 0;
+        // for (int i = 1; i <= arr.length+1; i++) {
+        //     xor1 = xor1^i;
+        //     System.out.println(xor1);
+        // }
+        int xor2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            xor2=xor2^arr[i];
+            xor1 = xor1^(i+1);
+        }
+        xor1=xor1^arr.length+1; // for last element  
+        return xor2^xor1;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 4, 5 };
-        System.out.println("The misiing element is : " + findMissingelementBetter(arr));
+        // System.out.println("The misiing element is : " + findMissingelementBetter(arr));
+        // System.out.println("The misiing element is : " + findMissingelementOptimal(arr));
+        System.out.println("The misiing element is : " + findMissingelementXor(arr));
     }
 }
